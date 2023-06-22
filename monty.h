677ea2053134bdd1232e58b26831137dfac11825
jerrynabango <jerrynabango9@gmail.com>
2023-06-23 02:35:03 +0300

    monty_update

--- monty.h
+++ monty.h
@@ -1,6 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
-
+#include "define.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -71,24 +71,6 @@ typedef stack_t dlistint_t;
 
 extern data_t info;
 
-#define DATA_INIT {NULL, NULL, NULL, NULL, 0}
-
-#define SUB_FAIL "L%u: can't sub, stack too short\n"
-#define DIV_FAIL "L%u: can't div, stack too short\n"
-#define DIV_ZERO "L%u: division by zero\n"
-#define MOD_FAIL "L%u: can't mod, stack too short\n"
-#define PCHAR_FAIL "L%u: can't pchar, stack empty\n"
-#define PCHAR_RANGE "L%u: can't pchar, value out of range\n"
-#define USAGE "USAGE: monty file\n"
-#define FILE_ERROR "Error: Can't open file %s\n"
-#define MUL_FAIL "L%u: can't mul, stack too short\n"
-#define PINT_FAIL "L%u: can't pint, stack empty\n"
-#define POP_FAIL "L%u: can't pop an empty stack\n"
-#define SWAP_FAIL "L%u: can't swap, stack too short\n"
-#define ADD_FAIL "L%u: can't add, stack too short\n"
-#define UNKNOWN "L%u: unknown instruction %s\n"
-#define MALLOC_FAIL "Error: malloc failed\n"
-#define PUSH_FAIL "L%u: usage: push integer\n"
 
 void _opcode(arg_stack *str);
 void (*get_func(char **parsed))(stack_t **, unsigned int);
